476f9abd47636e9f92a0593a29c550d260e85b65
nathansmj <nathansmj@gmail.com>
2018-07-07 21:38:04 +0530

    second version

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ global.approot = require('app-root-path')
 const Seats = require(global.approot + '/app/seats.js')
 
 let currentSeats = new Seats([[3,2],[4,3],[2,3],[3,4]]);
-currentSeats.allocatePassengers(10)
-currentSeats.allocatePassengers(30)
+currentSeats.displaySeatMap(10)
+currentSeats.displaySeatMap(30)
 
 currentSeats = new Seats([[2,2],[2,6], [4,4]]);
-currentSeats.allocatePassengers(15)
-currentSeats.allocatePassengers(25)
+currentSeats.displaySeatMap(15)
+currentSeats.displaySeatMap(25)
 
--- app/seats.js
+++ app/seats.js
@@ -12,138 +12,136 @@ const seatPriorityOrder = [
 ]
 class Seats {
     constructor (seats_spec) {
-        this.seats_spec = seats_spec;
-        this.seats_dimension = this.getSeatsDimension()
-        this.seats_type = this.getSeatsType() // 3D Array with
-        this.seats_by_priority = this.getSeatsPriorityList()
+     
+        this.seats_spec = seats_spec
+        this.seats_stat = {
+            'dimension': [],
+            'columns': {},
+            'block_start_columns': []
+        }
+        this.setSeatsStats()
+        this.seats_matrix = this.getSeatsMatrix()
+        this.seats_by_priority = this.getSeatsPriority()
     }
-    // to get max rows and columns
-    getSeatsDimension() {
-        let rows = []
-        let columns = []
+    isSeatsSpecValid(seats_spec) {
+        if(!seats_spec.length <2) {
+            return new Error('Seats specification whould have atleast two blocks of seats')
+        } 
+    }
+    // to seats dimension & Column types
+    setSeatsStats() {
+        let total_columns = 0
+        let total_rows = 0
         let total_block = this.seats_spec.length
+        let total_seats = 0
+        let block_start_column = 0
         for (let i=0; i < total_block; i++ ) {
-            columns.push(this.seats_spec[i][0])
-            rows.push(this.seats_spec[i][1])
-        }
-        let total_columns = columns.reduce((a, b) => a + b, 0)
-        let total_rows = Math.max(...rows)
-        return {
-            'columns':total_columns, 
-            'rows': total_rows
-        }
-    }
+            total_seats += (this.seats_spec[i][0] * this.seats_spec[i][1]) 
+            total_columns += this.seats_spec[i][0]
 
-    // Get 3D array with type of seats
-    getSeatsType() {
-        let total_block = this.seats_spec.length
-        let total_rows = this.seats_dimension.rows
-
-        let all_seats_details = []
-        for (let b=0; b < total_block; b++){
-            let seat_block = this.seats_spec[b]
-            let block_seats_detail = []
-            for (let i=0; i < total_rows; i++ ){
-                block_seats_detail[i] = block_seats_detail[i] || []
-                for (let j=0; j < seat_block[0]; j++){
-
-                    // if the block has the row
-                    if (i<seat_block[1]){
-                        // if left seat of block its a Window
-                        if (j == 0) {
-                            block_seats_detail[i][j] = (b == 0) ? seatTypeMap.WINDOW : seatTypeMap.AISLE
-                        } else if (j == (seat_block[0]-1)) {
-                            block_seats_detail[i][j] = (b == (total_block-1)) ? seatTypeMap.WINDOW : seatTypeMap.AISLE
-                        }else {
-                            block_seats_detail[i][j] = seatTypeMap.CENTER
-                        }
-                    }   else {
-                        block_seats_detail[i][j] = seatTypeMap.NOSEAT
-                    }
-                }
+            if (i !== total_block-1) {
+                block_start_column += (this.seats_spec[i][0])
+                this.seats_stat.block_start_columns.push(block_start_column)
+    
+                this.seats_stat.columns[total_columns] = seatTypeMap.AISLE
+                this.seats_stat.columns[total_columns-1] = seatTypeMap.AISLE
             }
-            all_seats_details.push(block_seats_detail)
+            total_rows = (this.seats_spec[i][1] > total_rows) 
+                ? this.seats_spec[i][1]
+                : total_rows
+
         }
-        return all_seats_details
+        this.seats_stat.columns[0] = seatTypeMap.WINDOW
+        this.seats_stat.columns[total_columns-1] = seatTypeMap.WINDOW
+        this.seats_stat.dimension = {
+            'columns':total_columns, 
+            'rows': total_rows,
+            'total_seats': total_seats
+        }
     }
 
-    // Get ordered listed  of seat positions based on priority
-    getSeatsPriorityList() {
-        if (!this.seats_type 
-            || this.seats_type.length == 0) {
-                
+    getSeatsMatrix() {
+        let seats_matrix = [[]]
+
+        // initate seats_matrix
+        let currentColumn = 0
+        for (let b=0; b < this.seats_spec.length; b++) {
+            let rows = this.seats_spec[b][1]
+            currentColumn += this.seats_spec[b][0]
+            for(let r = 0; r < this.seats_spec[b][1]; r++) {
+                seats_matrix[r] = seats_matrix[r] || []
+                for (let c = (currentColumn - this.seats_spec[b][0]); 
+                    c < currentColumn; 
+                    c++) {
+                        seats_matrix[r][c] = this.seats_stat.columns[c] || seatTypeMap.CENTER
+                    }
+            }
+        }
+        return seats_matrix
+    }
+
+    getSeatsPriority () {
+        if (this.seats_matrix.length === 0
+            || this.seats_stat.total_seats === 0) {
+       
             return
         }
-        let seat_order = {
-            'ALL': []
-        }
+        let seats_by_priority = {}
         seatPriorityOrder.forEach(function(type){
-            if (type === 'NOSEAT') return
-            seat_order[seatTypeMap[type]] = []
+            let seat_type = seatTypeMap[type]
+            seats_by_priority[seat_type] = []
         })
-        let total_block = this.seats_spec.length;
-        for (let r=0; r < this.seats_dimension.rows; r++) {
-            for (let b=0; b < total_block; b++) {
-                for (let c=0; c < this.seats_spec[b][0]; c++) {
-                    seatPriorityOrder.forEach(function(type){
-                        if (type === 'NOSEAT') return
-                        if (this.seats_type[b][r][c] === seatTypeMap[type]) {
-                            seat_order[seatTypeMap[type]].push([b, r, c])
-                        }
-                    }.bind(this)) 
+        for (let row = 0; row < this.seats_stat.dimension.rows; row++) {
+            for (let col = 0; col < this.seats_stat.dimension.columns; col++) {
+                let seat_type = this.seats_matrix[row][col]
+                if (seat_type) {
+                    seats_by_priority[seat_type].push([row,col])
                 }
             }
         }
+        seats_by_priority.ALL = []
         seatPriorityOrder.forEach(function(type){
-            seat_order.ALL = seat_order.ALL.concat(seat_order[seatTypeMap[type]])
+            let seat_type = seatTypeMap[type]
+            seats_by_priority.ALL = seats_by_priority.ALL.concat(seats_by_priority[seat_type])
         })
-        return seat_order
+        return seats_by_priority.ALL
     }
+    displaySeatMap (passenger_count) {
 
-    // print the formatted allocations
-    allocatePassengers (passenger_count) {
-        let seats = this.seats_type.slice(0)
+        // let passenger_count = this.seats_stat.dimension.total_seats
+        console.log('')
         console.log(`Passenger Count: ${passenger_count}`)
         console.log('Given Seat Spec: ' + JSON.stringify(this.seats_spec))
         console.log('---------------')
-        // console.log(this.seats_by_priority.ALL)
-        let seatsOrder = this.seats_by_priority.ALL
+        
+        let seats = this.seats_matrix.slice(0)
+        let seatsOrder = this.seats_by_priority
         for (let i=0; i < passenger_count; i++ ) {
             let current_seat = seatsOrder[i]
-            seats[current_seat[0]][current_seat[1]][current_seat[2]] = i+1
+            seats[current_seat[0]][current_seat[1]] = i+1
         }
-
-        let total_block = this.seats_spec.length;
-        for (let r=0; r < this.seats_dimension.rows; r++) {
-            for (let b=0; b < total_block; b++) {
-                for (let c=0; c < this.seats_spec[b][0]; c++) {
-                    if (seats[b][r][c] === seatTypeMap.NOSEAT) {
-                        process.stdout.write('   ')
-                    } else {
-                        let pass_str = this.pad(seats[b][r][c],2)
-                        process.stdout.write(pass_str+' ')                        
-                    }
+        for (let row = 0; row < this.seats_stat.dimension.rows; row++) {
+            for (let col = 0; col < this.seats_stat.dimension.columns; col++) {
+                let printStr = (seats[row][col]) ? seats[row][col] : ' '
+                printStr = this.pad(printStr, 2)
+                printStr += ' '
+                if (this.seats_stat.block_start_columns.includes(col)) {
+                    printStr = '| ' + printStr
                 }
-                process.stdout.write(' | ')
+                process.stdout.write(printStr)
             }
-            console.log(' ')
+            console.log('')
         }
-
     }
+
     pad(num, size) {
         let s
         if(Number.isInteger(num)) {
-            s = "000000000" + num;
+            s = "000000000" + num
         } else {
-            s = "---------" + num;
+            s = "---------" + num
         }
-        return s.substr(s.length-size);
-    }
-    validateSeatsSpec() {
-        return 1
-    }
-    getTotalSeats() {
-        return 1
+        return s.substr(s.length-size)
     }
 }
 
